d49d1eff1d1de37dec2b9f27d8c423c556583341
Steven Hahn <hahnse@ornl.gov>
2017-03-30 17:56:22 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name='licorne-py',
+    version='0.1.0',
+    description='',
+    author='Steven Hahn, Andrei Savici',
+    author_email='hahnse@ornl.gov, saviciat@ornl.gov',
+    url='https://github.com/neutrons/licorne-py',
+    license='GPLv3+',
+    packages=['licrone']
+)
